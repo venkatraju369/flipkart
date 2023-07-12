46dbf5e05c81fb1cc4afb37163ab004d3179d8fd
venkatraju369 <venkatraju3866@gmail.com>
2023-07-12 15:49:47 +0530

    firstupdate

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,11 @@ this lines  in dev batch
 this lines  in dev batch
 this lines  in dev batch
 this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
-this lines  in dev batch
+this lines in master branch 
+this lines in master branch
+this lines in master branch
+this lines in master branch
+this lines in master branch
+this lines in master branch
+this lines in master branch
 
--- string.sh
+++ string.sh
@@ -1,12 +1,14 @@
 this line contain string related codethis line contain string related code
 this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
-this line contain string related code
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
 
