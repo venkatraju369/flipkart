2b7502b1b4ef8a025685e96df70e3e2f0670a785
venkatraju369 <venkatraju3866@gmail.com>
2023-07-12 15:42:06 +0530

    firstupdat01

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,19 @@ this lines in master branch
 this lines in master branch
 this lines in master branch
 this lines in master branch
-this lines in master branch
+this lines in master bra
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+this lines  in dev batch
+
 
--- string.sh
+++ string.sh
@@ -1,14 +1,24 @@
 this line contain string related codethis line contain string related code
+this line contain string related 
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in master
+this line contain string related code in maste
 this line contain string related code
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
-this line contain string related code in master
+this line contain string related code
+this line contain string related code
+this line contain string related code
+this line contain string related code
+this line contain string related code
+this line contain string related code
+this line contain string related code
+this line contain string related code
+
 
